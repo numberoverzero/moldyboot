98b282bc7f4686860f002c8fb012b159ebb554bc
Joe Cross <joe.mcross@gmail.com>
2016-06-01 01:13:40 -0700

    test PublicKeyType load/dump

--- tests/models/test_key.py
+++ tests/models/test_key.py
@@ -1,4 +1,4 @@
-from gaas.models.key import Key
+from gaas.models.key import Key, PublicKeyType
 
 import arrow
 import base64
@@ -19,6 +19,15 @@ def generate_key():
     """Returns private, public"""
     pair = RSA.generate(1024)
     return pair, pair.publickey()
+
+
+def test_key_type():
+    _, public = generate_key()
+    serialized_public = base64.b64encode(public.exportKey(format="DER"))
+
+    key_type = PublicKeyType()
+    assert key_type.dynamo_dump(public) == serialized_public.decode("utf-8")
+    assert key_type.dynamo_load(serialized_public) == public
 
 
 def test_verify_success():
