522f6ce11dcfb72540eab43077da5f6d509d5573
Joe Cross <joe.mcross@gmail.com>
2016-06-01 17:11:21 -0700

    Mock bloop engine, test Key.load, Key.refresh, Key.revoke

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,47 @@
+import bloop
+import gaas.models
+import gaas.models.key
 import pytest
 from Crypto.PublicKey import RSA
+
+
+class MockEngine(bloop.Engine):
+    # Lack of client is intentional - there should be no downstream calls
+    def __init__(self, real_engine: bloop.Engine):
+        self.real_engine = real_engine
+        self.temp_config = []
+        # Don't call super().__init__ since we don't want to set up a real client
+
+    @property
+    def type_engine(self):
+        return self.real_engine.type_engine
+
+    @property
+    def model(self):
+        return self.real_engine.model
+
+    @property
+    def unbound_models(self):
+        return self.real_engine.unbound_models
+
+    @property
+    def models(self):
+        return self.real_engine.models
+
+    @property
+    def config(self):
+        return self.real_engine.config
+
+    def context(self, **config):
+        self.temp_config.append(config)
+        return self
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *exc):
+        self.temp_config.pop()
+        return False
 
 
 @pytest.fixture
@@ -25,3 +67,19 @@ def crypto_priv(crypto_pair):
 @pytest.fixture
 def crypto_pub(crypto_pair):
     return crypto_pair[1]
+
+
+@pytest.yield_fixture
+def mock_engine():
+    mock_engine = MockEngine(gaas.models.engine)
+    real_engine = gaas.models.engine
+
+    # Replace references where engine is already imported
+    gaas.models.engine = mock_engine
+    gaas.models.key.engine = mock_engine
+
+    yield mock_engine
+
+    # Undo patch
+    gaas.models.engine = real_engine
+    gaas.models.key.engine = real_engine
--- tests/models/test_key.py
+++ tests/models/test_key.py
@@ -1,7 +1,11 @@
+from gaas.models import NotFound
 from gaas.models.key import Key, PublicKeyType
 
 import arrow
 import base64
+import bloop
+import pytest
+import uuid
 
 
 def test_key_type(crypto_pair):
@@ -23,3 +27,78 @@ def test_expired():
     # key is valid until 5 seconds from now
     key = Key(until=now.replace(seconds=5))
     assert not key.expired
+
+
+def test_refresh(crypto_pub, mock_engine):
+    user_id = uuid.uuid4()
+    key_id = uuid.uuid4()
+    key = Key(user_id=user_id, key_id=key_id, public=crypto_pub)
+
+    called_save = 0
+
+    def mock_save(item, *, condition=None, atomic=None):
+        nonlocal called_save
+        called_save += 1
+
+        # Saving the key that's being refreshed
+        assert item is key
+        # Condition should be against key.until against a very recent date (~now)
+        assert condition.column is Key.until
+        now = arrow.now()
+        assert now.replace(seconds=-1) <= condition.value <= now.replace(seconds=1)
+        # As a mutating operation against a public key, refresh should always be atomic
+        assert atomic is True
+    mock_engine.save = mock_save
+
+    key.refresh()
+    assert called_save == 1
+
+
+def test_load(mock_engine):
+    user_id = uuid.uuid4()
+    key_id = uuid.uuid4()
+
+    called_load = 0
+
+    def mock_load(item, *, consistent=None):
+        nonlocal called_load
+        called_load += 1
+        assert item.user_id == user_id
+        assert item.key_id == key_id
+        assert consistent is True
+    mock_engine.load = mock_load
+
+    key = Key.load(user_id, key_id)
+    assert key.user_id == user_id
+    assert key.key_id == key_id
+    assert called_load == 1
+
+
+def test_load_missing(mock_engine):
+    user_id = uuid.uuid4()
+    key_id = uuid.uuid4()
+
+    def mock_load(item, consistent=None):
+        raise bloop.NotModified("load", [item])
+    mock_engine.load = mock_load
+
+    with pytest.raises(NotFound):
+        Key.load(user_id, key_id)
+
+
+def test_revoke(mock_engine):
+    user_id = uuid.uuid4()
+    key_id = uuid.uuid4()
+
+    called_delete = 0
+
+    def mock_delete(item, *, atomic=None):
+        nonlocal called_delete
+        called_delete += 1
+        assert item.user_id == user_id
+        assert item.key_id == key_id
+        assert atomic is True
+    mock_engine.delete = mock_delete
+
+    Key(user_id=user_id, key_id=key_id).revoke()
+    assert called_delete == 1
