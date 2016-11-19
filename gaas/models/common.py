a890e2b34b74428c28040bd6cff2f86f05d6975b
Joe Cross <joe.mcross@gmail.com>
2016-11-19 00:32:56 -0800

    Make models comparable, fixes failing unit tests

--- gaas/models/common.py
+++ gaas/models/common.py
@@ -1,3 +1,17 @@
-from bloop import BaseModel
+import bloop
 
 __all__ = ["BaseModel"]
+
+
+class BaseModel(bloop.BaseModel):
+    class Meta:
+        abstract = True
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return False
+        for column in self.Meta.columns:
+            name = column.model_name
+            if getattr(self, name, None) != getattr(other, name, None):
+                return False
+        return True
