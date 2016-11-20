f4a043ec2b61a667c5a269e87c9ad166efe931b6
Joe Cross <joe.mcross@gmail.com>
2016-11-20 09:44:52 -0800

    Support jwk private keys, handle url encoding, JWK padding

--- gaas/security/jwk.py
+++ gaas/security/jwk.py
@@ -1,18 +1,31 @@
 import base64
 import math
 
-from cryptography.hazmat.primitives.asymmetric.rsa import RSAPublicKey, RSAPublicNumbers
+from cryptography.hazmat.primitives.asymmetric.rsa import (
+    RSAPrivateKey,
+    RSAPrivateNumbers,
+    RSAPublicKey,
+    RSAPublicNumbers,
+    rsa_crt_iqmp,
+    rsa_crt_dmp1,
+    rsa_crt_dmq1
+)
 
 
 def i2b64(x: int) -> str:
     """Return the b64 encoding of the smallest number of bytes needed to represent the int"""
     length = math.ceil(x.bit_length() / 8)
     x_bytes = x.to_bytes(length, "big")
-    return base64.b64encode(x_bytes).decode("utf-8")
+    b = base64.b64encode(x_bytes).decode("utf-8")
+    return b.replace("+", "-").replace("/", "_")
 
 
 def b642i(b: str) -> int:
     """Return the big-endian integer represented by the bas64-encoded string"""
+    # fix url encoding
+    b = b.replace("-", "+").replace("_", "/")
+    # JWK drops padding
+    b += "=" * (len(b) % 4)
     x_bytes = base64.b64decode(b.encode("utf-8"))
     return int.from_bytes(x_bytes, "big")
 
@@ -27,4 +40,30 @@ def dump_public_key(key: RSAPublicKey) -> dict:
     return {
         "e": i2b64(numbers.e),
         "n": i2b64(numbers.n)
+    }
+
+
+def load_private_key(data, backend) -> RSAPrivateKey:
+    public_numbers = RSAPublicNumbers(e=b642i(data["e"]), n=b642i(data["n"]))
+    p = b642i(data["p"])
+    q = b642i(data["q"])
+    d = b642i(data["d"])
+    numbers = RSAPrivateNumbers(
+        p, q, d,
+        rsa_crt_dmp1(d, p),
+        rsa_crt_dmq1(d, q),
+        rsa_crt_iqmp(p, q),
+        public_numbers
+    )
+    return numbers.private_key(backend)
+
+
+def dump_private_key(key: RSAPrivateKey) -> dict:
+    numbers = key.private_numbers()
+    return {
+        "p": i2b64(numbers.p),
+        "q": i2b64(numbers.q),
+        "d": i2b64(numbers.d),
+        "e": i2b64(numbers.public_numbers.e),
+        "n": i2b64(numbers.public_numbers.n),
     }
--- tests/security/test_jwk.py
+++ tests/security/test_jwk.py
@@ -1,12 +1,22 @@
-from gaas.security.jwk import dump_public_key, load_public_key
+from gaas.security.jwk import (
+    dump_private_key,
+    dump_public_key,
+    load_private_key,
+    load_public_key,
+)
 
 from cryptography.hazmat.backends import default_backend
-
-from tests.helpers import as_der
 
 
 def test_public_key(rsa_pub):
     """round trip public key through jwk"""
     public_jwk = dump_public_key(rsa_pub)
     same_pub = load_public_key(public_jwk, backend=default_backend())
-    assert as_der(rsa_pub) == as_der(same_pub)
+    assert rsa_pub.public_numbers() == same_pub.public_numbers()
+
+
+def test_private_key(rsa_priv):
+    """round trip private key through jwk"""
+    private_jwk = dump_private_key(rsa_priv)
+    same_priv = load_private_key(private_jwk, backend=default_backend())
+    assert rsa_priv.private_numbers() == same_priv.private_numbers()
