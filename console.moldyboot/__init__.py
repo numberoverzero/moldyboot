a56551fe58454fb1c7dd0a697c219711ecca92d2
Joe Cross <joe.mcross@gmail.com>
2016-11-26 01:57:55 -0800

    Extract base html layout, convert html to use templates

--- console.moldyboot/__init__.py
+++ console.moldyboot/__init__.py
@@ -1,15 +1,36 @@
+import functools
 import os
 import pathlib
 from jinja2 import Environment, FileSystemLoader
 
+
+# ====================================================================================================== static config
+
+
+jinja_options = {
+    "lstrip_blocks": True,
+    "trim_blocks": True
+}
+
 src_root = pathlib.Path(
     os.path.abspath(os.path.dirname(__file__))
 ).resolve()
+sources = {
+    "css": ["css", "vendor/css"],
+    "js": ["js", "vendor/js"],
+    "html": ["html"]
+}
+
 loader = FileSystemLoader([
     str(src_root),
     str(src_root / "vendor")  # Find vendor/css/js/script as css/js/script
 ])
-env = Environment(loader=loader)
+env = Environment(loader=loader, **jinja_options)
+
+
+# ====================================================================================================== static config
+
+# ========================================================================================================== rendering
 
 
 def ensure_path_to(dst):
@@ -20,33 +41,66 @@ def ensure_path_to(dst):
         pass
 
 
-def render_to_directory(dst, ctx=None, dry_run=True, overwrite=False):
-    ctx = ctx or {}
-    dst_root = pathlib.Path(dst).resolve()
-
-    sources = {
-        "css": ["css", "vendor/css"],
-        "js": ["js", "vendor/js"],
-        "html": ["html"]
-    }
-
-    # 0. Collect all source files
-    files = []
+def iter_sources():
     for file_type, roots in sources.items():
         for root in roots:
             for filename in (src_root / root).glob("**/*." + file_type):
-                out = dst_root / file_type / filename.relative_to(src_root / root)
-                if not dry_run:
-                    ensure_path_to(out)
-                    out = out.resolve()
-                files.append((filename.resolve(), out))
+                yield file_type / filename.relative_to(src_root / root)
 
-    # 1. Render each to dst
-    for src, dst in files:
-        print("{} => {}".format(src, dst))
-        tpl = env.get_template(str(src.relative_to(src_root)))
+
+def iter_rendered(ctx=None):
+    ctx = extend_context(ctx)
+    for src in iter_sources():
+        tpl = env.get_template(str(src))
         rendered = tpl.render(ctx)
+        yield src, rendered
+
+
+def render_to_directory(dst_root, ctx=None, dry_run=True, overwrite=False):
+    dst_root = pathlib.Path(dst_root)
+    ctx = extend_context(ctx)
+    for src, rendered in iter_rendered(ctx):
+        dst = dst_root / src
         if not dry_run:
-            mode = "w" if overwrite else "x"  # x fails if dst exists
+            ensure_path_to(dst)
+            mode = "w" if overwrite else "x"  # "x" fails if dst exists
             with dst.open(mode=mode, encoding="utf-8") as dst_file:
                 dst_file.write(rendered)
+
+
+# ========================================================================================================== rendering
+
+# ==================================================================================================== context helpers
+
+
+def extend_context(ctx=None):
+    if ctx is None:
+        ctx = {}
+    for file_type in ["css", "js", "html"]:
+        ctx[file_type + "_file"] = functools.partial(url_file, file_type=file_type)
+    return ctx
+
+
+def url_file(name, file_type):
+    if str(name).endswith("." + file_type):
+        name = name[:1 + len(file_type)]
+    return "/{type}/{name}.{type}".format(name=name, type=file_type)
+
+
+# ==================================================================================================== context helpers
+
+# context = {
+#     "endpoints": {
+#         "api": "https://api.moldyboot.com",
+#         # https://console.moldyboot.com
+#         # since these are rendered on console.moldyboot, they can all be relative
+#         "console": ""
+#     },
+#     "webcrypto": {
+#         "databaseName": "MoldyDatabase",
+#         "databaseVersion": 1,
+#         "keyStoreName": "MoldyKeyStore",
+#         "metaStoreName": "MoldyMetaStore"
+#     }
+# }
+# render_to_directory("output-test", context, False, True)
